950e9405ad769465e185e53083761fd4af4b118b
Mark Seminatore <mseminatore@users.noreply.github.com>
2020-09-27 10:38:31 -0700

    Update shBrushVerilog.js
    Removed special string case

--- shBrushVerilog.js
+++ shBrushVerilog.js
@@ -17,8 +17,7 @@
 		this.regexList = [
 			{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },			// one line comments
 			{ regex: SyntaxHighlighter.regexLib.multiLineCComments,		css: 'comments' },			// multiline comments
-            { regex: /(R|L|U|u|u8)?"([^\\"\n]|\\.)*"/g,                 css: 'string' },			// special character
-            { regex: /(R|L|U|u|u8)?'([^\\'\n]|\\.)*'/g,                 css: 'string' },			// special string
+		        { regex: /(R|L|U|u|u8)?"([^\\"\n]|\\.)*"/g,                 	css: 'string' },			// special character
 			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },			// strings
 			{ regex: new RegExp(this.getKeywords(datatypes), 'gm'),		css: 'color1 bold' },
 			{ regex: new RegExp(this.getKeywords(functions), 'gm'),		css: 'functions bold' },
