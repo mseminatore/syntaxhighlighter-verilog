3f531448e8081ea69a9515918eb8a0b00fe4e91f
Mark Seminatore <mseminatore@users.noreply.github.com>
2020-09-29 11:46:27 -0700

    Update syntaxhighlighter-verilog.php

--- syntaxhighlighter-verilog.php
+++ syntaxhighlighter-verilog.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: SyntaxHighlighter Evolved: Verilog Brush
+Plugin Name: Add Verilog Brush to SyntaxHighlighter Evolved
 Description: Adds support for the Verilog language to the SyntaxHighlighter Evolved plugin.
 Author: Mark Seminatore
-Version: 1.0.5
+Version: 1.0.6
 Author URI: https://fpgacoding.com
 */
  
@@ -16,7 +16,7 @@ add_filter( 'syntaxhighlighter_brush_names', 'syntaxhighlighter_verilog_addname'
 
 // Register the brush file with WordPress
 function syntaxhighlighter_verilog_regscript() {
-    wp_register_script( 'syntaxhighlighter-brush-verilog', plugins_url( 'shBrushVerilog.js', __FILE__ ), array('syntaxhighlighter-core'), '1.0.5', true );
+    wp_register_script( 'syntaxhighlighter-brush-verilog', plugins_url( 'shBrushVerilog.js', __FILE__ ), array('syntaxhighlighter-core'), '1.0.6', true );
 }
  
 // Filter SyntaxHighlighter Evolved's language array
